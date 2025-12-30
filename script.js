cdf7a4cda18e1f35b5ee603d81d97520a4cc916f
Blackpop138 <Uncle3581@gmail.com>
2025-12-30 13:04:35 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-const container = document.getElementById("anime-container");
+let page = 1;
+let loading = false;
 
-fetch("https://api.jikan.moe/v4/top/anime")
-.then(res => res.json())
-.then(data => {
-data.data.slice(0, 12).forEach(anime => {
+async function loadAnime() {
+if (loading) return;
+loading = true;
+
+const res = await fetch(
+`fetch("http://localhost:3000/api/anime/top?page=1")
+const data = await res.json();
+
+data.data.forEach(anime => createCard(anime));
+page++;
+loading = false;
+}
+
+function createCard(anime) {
 const card = document.createElement("div");
 card.className = "anime-card";
-
 card.innerHTML = `
 <img src="${anime.images.jpg.image_url}">
 <h3>${anime.title}</h3>
 `;
-
+card.onclick = () => {
+window.location.href = `anime.html?id=${anime.mal_id}`;
+};
 container.appendChild(card);
+}
+
+window.addEventListener("scroll", () => {
+if (
+window.innerHeight + window.scrollY >=
+document.body.offsetHeight - 200
+) {
+loadAnime();
+}
 });
-});
+
+loadAnime();
