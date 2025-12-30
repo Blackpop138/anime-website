d3662bf154819a810666a98ee1cc6157e67bc431
Blackpop138 <Uncle3581@gmail.com>
2025-12-30 13:39:28 -0500

    Delete profile.js

--- profile.js
+++
@@ -1,8 +0,0 @@
-const favs = JSON.parse(localStorage.getItem("favorites")) || [];
-const container = document.getElementById("favorites");
-
-favs.forEach(a => {
-const div = document.createElement("div");
-div.innerHTML = `<img src="${a.image}"><p>${a.title}</p>`;
-container.appendChild(div);
-});
